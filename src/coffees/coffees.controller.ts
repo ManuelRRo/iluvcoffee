505a0a926911696e7a8d8a90d3aa91a9643ef091
ManuelRRo <ds23906@gmail.com>
2025-06-11 11:13:43 -0600

    coffees: service,entities

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -8,29 +8,31 @@ import {
   Post,
   Query,
 } from '@nestjs/common';
+import { CoffeesService } from './coffees.service';
 
 @Controller('coffees')
 export class CoffeesController {
+  constructor(private readonly coffeeService: CoffeesService) {}
   @Get()
   findAll(@Query() paginationQuery) {
-    const { offset, limit } = paginationQuery;
-    return `This action returns all coffee limit:${limit} and offset:${offset}`;
+    // const { offset, limit } = paginationQuery;
+    return this.coffeeService.findAll();
   }
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return `This action returns #${id} coffee`;
+    return this.coffeeService.findOne(id);
   }
   @Post()
   create(@Body() body) {
-    return body;
+    return this.coffeeService.create(body);
   }
   @Patch(':id')
   update(@Param('id') id: string, @Body() body) {
-    return `This action updates #${id} coffee`;
+    return this.coffeeService.update(id, body);
   }
 
   @Delete(':id')
   remove(@Param('id') id: string) {
-    return `This action removes #${id} coffee`;
+    return this.coffeeService.remove(id);
   }
 }
