0ecf01cae22d6ddbd6aebd0ae8590daee52ddb8a
ManuelRRo <ds23906@gmail.com>
2025-06-11 10:39:32 -0600

    add basic CRUD controllers with paginatioon

---
+++ src/coffees/coffees.controller.ts
@@ -0,0 +1,36 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
+
+@Controller('coffees')
+export class CoffeesController {
+  @Get()
+  findAll(@Query() paginationQuery) {
+    const { offset, limit } = paginationQuery;
+    return `This action returns all coffee limit:${limit} and offset:${offset}`;
+  }
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return `This action returns #${id} coffee`;
+  }
+  @Post()
+  create(@Body() body) {
+    return body;
+  }
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() body) {
+    return `This action updates #${id} coffee`;
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return `This action removes #${id} coffee`;
+  }
+}
